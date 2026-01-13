cfd5c798111e1075d31b82919b0b420713f0f8df
hinterbergers <hinterbergers@me.com>
2026-01-13 22:47:00 +0100

    make drizzle.config.ts work with npx drizzle-kit

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,10 +1,8 @@
-import { defineConfig } from "drizzle-kit";
-
-export default defineConfig({
+export default {
   schema: "./shared/schema.ts",
   out: "./drizzle",
   dialect: "postgresql",
   dbCredentials: {
     url: process.env.DATABASE_URL!,
   },
-});
+};
